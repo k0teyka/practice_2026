82a9b3f70e7098bb4c8d301e43c20876b599789b
k0teyka <maksim.lukyanov.2007@mail.ru>
2026-07-02 06:54:58 +0600

    add task01 source code and tests

---
+++ task01/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace task01;
+
+public static class StringExtensions
+{
+    public static bool IsPalindrome(this string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return false;
+        }
+
+        var sb = new StringBuilder();
+        
+        foreach (char c in input.ToLower())
+        {
+            if (!char.IsPunctuation(c) && !char.IsWhiteSpace(c))
+            {
+                sb.Append(c);
+            }
+        }
+
+        string cleaned = sb.ToString();
+
+        if (cleaned.Length == 0)
+        {
+            return false;
+        }
+
+        char[] charArray = cleaned.ToCharArray();
+        Array.Reverse(charArray);
+        string reversed = new string(charArray);
+
+        return cleaned == reversed;
+    }
+}
